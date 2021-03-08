5bdfae1b709594287b8abff86f526277ee78c696
luhavis <luhavisdev@gmail.com>
2021-03-09 01:57:34 +0900

    Feat :  init react and es6 (#1)

---
+++ do-it-es6/01.js
@@ -0,0 +1,25 @@
+var string1 = '안녕하세요';
+var string2 = '반갑습니다';
+var greeting = string1 + ' ' + string2;
+var produect = { name: '도서', price: '31900원' };
+var message = '제품' + produect.name + '의 가격은' + produect.price + '입니다';
+var multiLine = '문자열1\n문자열2'; 
+var value1 = 1;
+var value2 = 2;
+var boolValue = false;
+var operator1 = '곱셈값은 ' + (value1 * value2) + '입니다.';
+var operator2 = '불리언값은 ' + (boolValue ? '참' : '거짓') + '입니다.';
+
+
+console.log(string1);
+console.log(string2);
+console.log(greeting);
+console.log(produect);
+console.log(message);
+console.log(multiLine);
+console.log(value1);
+console.log(value2);
+console.log(boolValue);
+console.log(operator1);
+console.log(operator2);
+
