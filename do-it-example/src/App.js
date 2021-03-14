489a8b0914b9d1c8bec4c1d9f273639da6ed1812
luhavis <luhavis.dev@gmail.com>
2021-03-14 23:33:34 +0900

    (#2)

--- do-it-example/src/App.js
+++ do-it-example/src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
+import TodaysPlan from './components/TodaysPlan';
 import './App.css';
 
-
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <h1 className="title">두잇! 리액트 시작하기</h1>
+      // 아래의 내용이 JSX 양식 입니다.
+      <div>
+        <img src="http://www.easyspub.co.kr/images/logo_footer.png" />
+        <div>안녕하세요</div>
+
+        <TodaysPlan />
       </div>
     );
   }
 }
-
 export default App;
---
+++ do-it-example/src/components/TodaysPlan.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class TodaysPlan extends React.Component {
+  render() {
+    return <div className="message-container">메세지!</div>;
+  }
+}
+
+export default TodaysPlan;
