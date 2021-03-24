0355720d2efcdcb2ed973b7f33bf87bd775143f4
luhavis <luhavis.dev@gmail.com>
2021-03-25 00:53:23 +0900

    Feat : reducer redux (#7)

--- do-it-example/src/stories/ReduxStory.jsx
+++ do-it-example/src/stories/ReduxStory.jsx
@@ -7,6 +7,8 @@ import ReduxApp03 from '../components/Redux03';
 import AdvReduxApp01 from '../components/AdvReduxApp01';
 import AdvReduxApp02 from '../components/AdvReduxApp02';
 import AdvReduxApp03 from '../components/AdvReduxApp03';
+import AdvReduxApp04 from '../components/AdvReduxApp04';
+import AdvReduxApp05 from '../components/AdvReduxApp05';
 
 storiesOf('ReduxApp', module)
   .addWithJSX('기본 스토어 설정', () => <ReduxApp01 />)
@@ -15,4 +17,6 @@ storiesOf('ReduxApp', module)
   .addWithJSX('다중 리듀서 설정', () => <AdvReduxApp01 />)
   .addWithJSX('다중 액션 설정', () => <AdvReduxApp02 />)
 
-  .addWithJSX('배열 액션 호출', () => <AdvReduxApp03 />);
+  .addWithJSX('배열 액션 호출', () => <AdvReduxApp03 />)
+  .addWithJSX('그래프DB 변환 작업', () => <AdvReduxApp04 />)
+  .addWithJSX('그래프DB 수정 액션 호출', () => <AdvReduxApp05 />);
