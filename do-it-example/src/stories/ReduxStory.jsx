c168786edd9d2e15cf5d1d39695e2aa91dcdba22
luhavis <luhavis.dev@gmail.com>
2021-03-25 00:05:45 +0900

    Feat : reducer redux (#7)

--- do-it-example/src/stories/ReduxStory.jsx
+++ do-it-example/src/stories/ReduxStory.jsx
@@ -3,7 +3,9 @@ import { storiesOf } from '@storybook/react';
 
 import ReduxApp01 from '../components/Redux01';
 import ReduxApp02 from '../components/Redux02';
+import ReduxApp03 from '../components/Redux03';
 
 storiesOf('ReduxApp', module)
   .addWithJSX('기본 스토어 설정', () => <ReduxApp01 />)
-  .addWithJSX('기본 액션 호출', () => <ReduxApp02 />);
+  .addWithJSX('기본 액션 호출', () => <ReduxApp02 />)
+  .addWithJSX('기본 리듀서 구현', () => <ReduxApp03 />);
