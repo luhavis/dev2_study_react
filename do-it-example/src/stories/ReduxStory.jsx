5da02b0c933b8e20d949c42bab69c4130c9ba515
luhavis <luhavis.dev@gmail.com>
2021-03-24 23:54:31 +0900

    Feat : action redux (#7)

--- do-it-example/src/stories/ReduxStory.jsx
+++ do-it-example/src/stories/ReduxStory.jsx
@@ -2,5 +2,8 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 
 import ReduxApp01 from '../components/Redux01';
+import ReduxApp02 from '../components/Redux02';
 
-storiesOf('ReduxApp', module).addWithJSX('기본 스토어 설정', () => <ReduxApp01 />);
+storiesOf('ReduxApp', module)
+  .addWithJSX('기본 스토어 설정', () => <ReduxApp01 />)
+  .addWithJSX('기본 액션 호출', () => <ReduxApp02 />);
