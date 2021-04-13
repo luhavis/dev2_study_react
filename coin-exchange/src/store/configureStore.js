1767c30229c6a608aa9405edf9ef8977e25c094b
luhavis <luhavis.dev@gmail.com>
2021-04-14 00:24:17 +0900

    Feat : custom middleware (#8)

--- coin-exchange/src/store/configureStore.js
+++ coin-exchange/src/store/configureStore.js
@@ -1,6 +1,16 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import reducers from "../reducers";
 
+const customMiddleware = (store) => (nextRunner) => (action) => {
+  console.log("액션 개체", action);
+  console.log("저장소 상태", store.getStore());
+  return nextRunner(action);
+};
+
 export default (initState) =>
-  createStore(combineReducers(reducers), initState, composeWithDevTools());
+  createStore(
+    combineReducers(reducers),
+    initState,
+    applyMiddleware(customMiddleware)
+  );
