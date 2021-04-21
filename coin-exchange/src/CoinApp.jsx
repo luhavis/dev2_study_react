e9d8172fa71f760f37f7ed3f7d5505c21bfdad52
luhavis <luhavis.dev@gmail.com>
2021-04-22 00:02:25 +0900

    Feat : add react router (#8)

--- coin-exchange/src/CoinApp.jsx
+++ coin-exchange/src/CoinApp.jsx
@@ -1,5 +1,6 @@
 import React, { PureComponent } from "react";
 import { Provider } from "react-redux";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import PropTypes from "prop-types";
 import AppLayout from "./components/AppLayout";
@@ -10,19 +11,24 @@ import TransactionList from "./components/TransactionList";
 import ModalProvider from "./ModalProvider";
 import NotificationContainer from "./containers/NotificationContainer";
 import TransactionListContainer from "./containers/TransactionListContainer";
+import NotFound from "./components/NotFound";
 
 class CoinApp extends PureComponent {
   store = configureStore();
   render() {
     return (
       <Provider store={this.store}>
-        <ModalProvider>
-          <AppLayout>
-            <CoinOverview />
-            <TransactionListContainer />
-            <NotificationContainer />
-          </AppLayout>
-        </ModalProvider>
+        <Router>
+          <ModalProvider>
+            <AppLayout>
+              <Switch>
+                <Route path="/" exact render={() => <MainPage />} />
+                <Route path="*" component={NotFound} />
+              </Switch>
+              <NotificationContainer />
+            </AppLayout>
+          </ModalProvider>
+        </Router>
       </Provider>
     );
   }
